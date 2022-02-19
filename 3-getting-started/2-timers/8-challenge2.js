84fe198f21e8d2d45b5455d0e7469e9113943f45
Pranav <prnvbirajdar@gmail.com>
2022-02-20 01:47:11 +0530

    challenge 2

--- 3-getting-started/2-timers/8-challenge2.js
+++ 3-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,15 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+
+let count = 0;
+
+const interval = setInterval(() => {
+  console.log("Hello World");
+  count++;
+
+  if (count === 5) {
+    console.log("Done");
+    clearInterval(interval);
+  }
+}, 1000)
